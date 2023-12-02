8bf694edd3a3d8ceea24211b429b3084a64adf31
lucasjerez94 <lucasjerez9468@gmail.com>
2023-12-02 18:41:44 -0300

    Changed CSS, margin continer

--- src/molecules/Sidebar.jsx
+++ src/molecules/Sidebar.jsx
@@ -9,6 +9,14 @@ export const Sidebar = ({}) => {
           <li><a href="#">Enlace 1</a></li>
           <li><a href="#">Enlace 2</a></li>
           <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 3</a></li>
         </ul>
       </div>
     );
