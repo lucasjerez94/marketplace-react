3e5c3520b17615f03cf109a16f5e0dc316be4b6f
lucasjerez94 <lucasjerez9468@gmail.com>
2023-12-13 15:20:59 -0300

    styles button

--- src/molecules/Header.jsx
+++ src/molecules/Header.jsx
@@ -4,14 +4,16 @@ export const Header = () => {
     return(
         <div className="header-container">
             <h1 className="h1-header">MarketPlace</h1>
-            <nav className="navbar-header">
-                <ul className="ul-header">
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Contacto</a></li>
-                    <li><a href="#">Nosotros</a></li>
-                </ul>
-            </nav>
-            <Button />
+            <div className='nav-container'>
+                <nav className="navbar-header">
+                    <ul className="ul-header">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Contacto</a></li>
+                        <li><a href="#">Nosotros</a></li>
+                    </ul>
+                </nav>
+                <Button />
+            </div>
         </div>
     );
 };
