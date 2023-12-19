76db0243fd4cb60376876cf52528c8dbca532eb9
lucasjerez94 <lucasjerez9468@gmail.com>
2023-12-19 17:39:39 -0300

    already

--- src/atoms/TemplateCard.jsx
+++ src/atoms/TemplateCard.jsx
@@ -1,15 +1,16 @@
 import { useStyles } from '../StylesContext';
 
-const TemplateCard = ({title, content, imageSrc}) => {
-
+const TemplateCard = ({ title, content, imageSrc }) => {
   const { styles } = useStyles();
 
   return (
     <div className="templatecard-container" style={{ background: styles.backgroundColor, color: styles.textColor }}>
       <div className="card-container">
-        <img className="card-image" src={imageSrc} alt={title}/>
+        <div className="image-container">
+          <img className="card-image" src={imageSrc} alt={title} />
+        </div>
         <h3 className="h3-templatecard">{title}</h3>
-        <p className= "p-templatecard">{content}</p>
+        <p className="p-templatecard">{content}</p>
       </div>
     </div>
   );
